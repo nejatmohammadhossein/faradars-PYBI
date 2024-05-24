48a71f64f4b9ec09b0d7832839882a0f55bc66bb
nejatmohammadhossein@gmail.com <nejatmohammadhossein@gmail.com>
2024-05-24 12:28:50 +0330

    adding a picture

--- app.py
+++ app.py
@@ -8,8 +8,3 @@ import plotly.graph_objects as go
 #Read
 df = pd.read_excel("Superstore.xlsx")
 
-#Show output
-image = Image.open("faradars.png")
-col1, col2 = st.columns([0.1,0.9])
-with col1:
-    st.image(image,width=20)
